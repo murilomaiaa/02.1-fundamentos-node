ac9b1740dd8c1c7009c38a0acc06a5af6b5023de
Murilo Maia <murilomaia.bb@gmail.com>
2020-06-27 00:59:43 -0300

    refactoring exceptions for the services

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -14,6 +14,21 @@ class CreateTransactionService {
   }
 
   execute({ title, value, type }: Request): Transaction {
+    if (type !== 'income' && type !== 'outcome') {
+      throw new Error('title must be "income" or "outcome"');
+    }
+
+    if (value <= 0) {
+      throw new Error('value must be greater than 0');
+    }
+
+    if (
+      type === 'outcome' &&
+      value > this.transactionsRepository.getBalance().total
+    ) {
+      throw new Error('insufficient funds');
+    }
+
     const transaction = this.transactionsRepository.create({
       title,
       value,
